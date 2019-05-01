ecbed0aa0a59d88360f009a7e8281894bdb8315a
HristoKehayov <hristokehayov@gmail.com>
2019-05-01 21:31:02 +0300

    05. DICTIONARIES, LAMBDA AND LINQ

---
+++ 05. DICTIONARIES, LAMBDA AND LINQ/Dict_Lambda_Linq_Lab/01. Odd_Occurrences/Odd_Occurrences.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Odd_Occurrences
+{
+    class Odd_Occurrences
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            string[] TextWords = text.Split(' ');
+            Dictionary<string, int> dictWords = new Dictionary<string, int>();
+            
+            for (int i = 0; i < TextWords.Length; i++)
+            {
+                int word_RepetitionCunter = 0;
+                string currentWordtocheck = TextWords[i];
+                foreach (var item in TextWords)
+                {
+                    if (item == currentWordtocheck)
+                    {
+                        word_RepetitionCunter++;
+                    }
+                }
+
+                if (!dictWords.ContainsKey(currentWordtocheck))
+                {
+                    dictWords.Add(currentWordtocheck, word_RepetitionCunter);
+                }
+
+            }
+
+            foreach (KeyValuePair<string, int> pair in dictWords)
+            {
+                if (pair.Value % 2 != 0)
+                {
+                    Console.WriteLine(pair.Key);
+                }
+            }
+            
+        }
+    }
+}
---
+++ 05. DICTIONARIES, LAMBDA AND LINQ/Dict_Lambda_Linq_Lab/02. CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CountRealNumbers
+{
+    class CountRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            string allNumbers = Console.ReadLine().ToLower();
+            string[] string_Numbers = allNumbers.Split(' ');
+            double[] myInts = Array.ConvertAll(string_Numbers, double.Parse);
+            SortedDictionary<double, int> dictWords = new SortedDictionary<double, int>();
+            for (int i = 0; i < myInts.Length; i++)
+            {
+                int number_RepetitionCunter = 0;
+                double currentNumbertocheck = myInts[i];
+                foreach (var item in myInts)
+                {
+                    if (item == currentNumbertocheck)
+                    {
+                        number_RepetitionCunter++;
+                    }
+                }
+
+                if (!dictWords.ContainsKey(currentNumbertocheck))
+                {
+                    dictWords.Add(currentNumbertocheck, number_RepetitionCunter);
+                }
+
+            }
+            foreach (KeyValuePair<double, int> pair in dictWords)
+                Console.WriteLine($"{pair.Key} -> {pair.Value} times");
+
+        }
+    }
+}
